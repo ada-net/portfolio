5b56a23be601d4c08c98cf69fcf3fa04206f9dfc
Sabine <81630944+ada-net@users.noreply.github.com>
2021-07-22 17:08:39 +0200

    modification brief section + js experiments
    changed id brief-points in class
    
    in the brief section, now every line has become a <p> element with class .brief-points assigned
    
    the change permits iterating through a for loop in the .js "playground" file -> on mouseover every single line turns bold and on mouseleave it turns normal.
    
    some other experiments in the .js file

--- playground.js
+++ playground.js
@@ -1,3 +1,23 @@
 const modifiedH1 = document.querySelector('H1');
-
 modifiedH1.style.color = 'purple';
+
+const picColor = document.querySelector('#picture');
+
+picColor.addEventListener('mouseover',function(){picColor.style.background='linear-gradient(rgb(100, 52, 190),ghostwhite)' });
+picColor.addEventListener('mouseleave',function(){picColor.style.background='linear-gradient(ghostwhite, rgb(100, 52, 190))' });
+
+
+const fatOnMouseOver = document.querySelectorAll('.brief-points');
+for (let i = 0; i < fatOnMouseOver.length; i++) {
+    fatOnMouseOver[i].addEventListener('mouseover', function() 
+    {
+      fatOnMouseOver[i].style.fontWeight='bolder';
+    });
+    fatOnMouseOver[i].addEventListener('mouseout', function()
+    {
+        fatOnMouseOver[i].style.fontWeight='normal';
+    });
+
+    };
+
+
